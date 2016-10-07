4431655a5982aa624c216c1f4d3abbe217f2716f
Zeotyn <dev@zeotyn.de>
2016-10-07 16:20:52 +0200

    wip

---
+++ ReactiveTest/RecipeViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeViewModel.swift
+//  ReactiveTest
+//
+//  Created by Eugen Waldschmidt on 07.10.16.
+//  Copyright © 2016 Eugen Waldschmidt. All rights reserved.
+//
+
+import Foundation
+
+class RecipeViewModel {
+    var base:Float = 0.0
+    var flavour:Float = 0.0
+    
+    var result:String {
+        get {
+            return "Nothing to do here..."
+        }
+    }
+}
